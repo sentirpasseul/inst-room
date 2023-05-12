fa14c2e0a2c1319a3980bd6ee3b4e1811f7f8bd0
Dmitrii <ultra.las664@gmail.com>
2023-05-12 14:19:24 +0300

    Добавлен компонент для элемента таблицы

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,24 @@
 export function CartPage() {
-    return (
-        <>
-        
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <div className="cart__header">
+          <span className="cart__header_title"></span>
+          <button className="cart__header_button"></button>
+        </div>
+        <div className="cart__table">
+          <div className="cart__table_header">
+            <span>Название</span>
+            <span>Стоимость</span>
+            <span>Количество</span>
+            <span>Итого</span>
+          </div>
+          <div className="cart__table_items">
+            
+          </div>
+          <div className="cart__table_summary"></div>
+        </div>
+      </div>
+    </>
+  );
 }
